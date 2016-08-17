05374ad612bb1a14a7b499ba4a1a0dc491441b34
Mathieu Roblin <roblin.mathieu@gmail.com>
2016-08-17 10:55:30 +0800

    ADD project sturcture using lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to uber-server
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/uber_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns uber-server.core-test
+  (:require [clojure.test :refer :all]
+            [uber-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
