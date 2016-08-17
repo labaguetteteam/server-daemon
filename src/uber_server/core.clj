6b4d3c5d3af71db10a95e8cd9fc84f7bbd4ab424
Mathieu Roblin <roblin.mathieu@gmail.com>
2016-08-17 15:31:27 +0800

    ADD async http lib to project

--- src/uber_server/core.clj
+++ src/uber_server/core.clj
@@ -1,7 +1,23 @@
 (ns uber-server.core
-  (:gen-class))
+  (:require [org.httpkit.client :as http]))
+
+; (def options {:timeout 200             ; ms
+;               :basic-auth ["user" "pass"]
+;               :query-params {:param "value" :param2 ["value1" "value2"]}
+;               :user-agent "User-Agent-string"
+;               :headers {"X-Header" "Value"}})
+
+(def options {})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Run")
+  (http/get "https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823" options
+          (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+            (if error
+              (println "Failed, exception is " error)
+              (do
+                (println "Async HTTP GET: " status)
+                (println "Body: " body)))))
+  (loop [] (recur)))
